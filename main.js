5a814ee4b35fc3737662bdfcf74fb9ee88ccdfe9
Taku <parrucciluciano@gmail.com>
2026-04-23 17:53:04 -0300

    Agregar archivo main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const dia = 'martes';
+switch (dia) {
+  case 'lunes':
+    console.log('Inicio de semana');
+    break;
+  case 'martes':
+    console.log('Segundo día');
+    break;
+  default:
+    console.log('Otro día');
+}
